8e1dd5d07598727de78ae0a791af590fe86d4d47
iambrozjak <ivanochkavasyljuk@gmail.com>
2024-01-01 23:11:02 +0200

    go to finish

--- js/admin.js
+++ js/admin.js
@@ -14,42 +14,21 @@ document.getElementById('adminForm').addEventListener('submit', function(event) 
     return;
   }
 
-  let messageElement = document.createElement('div');
-  messageElement.innerHTML = `
-  <div class="card">
-  <img src="${photoInput}" alt="News Photo">
-  <div class="card-container">
-    <h4>${messageTitle}</h4>
-    <p>${messageText}</p>
-  </div>
-  </div>
+  const message = {
+    img: photoInput,
+    title: messageTitle,
+    text: messageText
+  };
 
-  <hr class="divider">`;
+  let messages = JSON.parse(localStorage.getItem('messages')) || [];
+  messages.push(message);
+  localStorage.setItem('messages', JSON.stringify(messages));
 
-  // alert("Ваше повідомлення опубліковане.");
+  alert("Ваша новина  збережина в локальному сховищі.");
+
+
   document.getElementById('photoInput').value = '';
   document.getElementById('newsText').value = '';
   document.getElementById('titleText').value = '';
-
-  function isOnline() {
-    return window.navigator.onLine;
-  }
-  if (isOnline()){
-    document.getElementById('messages').prepend(messageElement);
-    alert("Ваше новина опублікована.");
-  } else {
-    // save message to local storage
-    const message = {
-      Image: photoInput,
-      text: messageTitle,
-      text: messageText
-    };
-
-    let messages = JSON.parse(localStorage.getItem('messages')) || [];
-    messages.push(message);
-    localStorage.setItem('messages', JSON.stringify(messages));
-
-    alert("Ваша новина  збережина в локальному сховищі.");
-  }
 
 })
--- js/news.js
+++ js/news.js
@@ -1,57 +1,36 @@
 function isOnline() {
   return window.navigator.onLine;
 }
-if (isOnline()){
-  alert("Ваше новина опублікована.");
-} else {
-  // save message to local storage
-  const message = {
-    img: photoInput,
-    text: messageTitle,
-    text: messageText
+document.addEventListener('DOMContentLoaded', function () {
+  if (isOnline()) {
+    displayNewsFromLocalStorage();
+  }
+
+  // event listener for online event
+  window.addEventListener('online', displayNewsFromLocalStorage);
+});
+function displayNewsFromLocalStorage(){
+
+    let messages = JSON.parse(localStorage.getItem("messages")) || [];
+    messages.forEach((message) => {
+      const photoInput = message.img;
+      const messageTitle = message.title;
+      const messageText = message.text;
+
+      let messageElement = document.createElement('div');
+      messageElement.innerHTML = `
+        <div class="card">
+          <img src="${photoInput}" alt="News Photo">
+          <div class="card-container">
+            <h4>${messageTitle}</h4>
+            <p>${messageText}</p>
+          </div>
+        </div>
+        <hr class="divider">
+      `;
+
+      document.getElementById('newsNew').prepend(messageElement);
+    });
+  // Очищення локального сховища після виведення повідомлень
+    localStorage.removeItem("messages");
   };
-
-  let messages = JSON.parse(localStorage.getItem('messages')) || [];
-  messages.push(message);
-  localStorage.setItem('messages', JSON.stringify(messages));
-
-  alert("Ваша новина  збережина в локальному сховищі.");
-}
-
-// function isOnline() {
-//   return window.navigator.onLine;
-// }
-// window.addEventListener("online", () => {
-//   console.log("я тут");
-//   let newsContainer = document.querySelector("news");
-
-//   let messages = JSON.parse(localStorage.getItem("messages")) || [];
-//   messages.forEach((message) => {
-//     const photoInput = document.getElementById('photoInput');
-//     const messageTitle = document.getElementById('titleText').value.trim();
-//     const messageText = document.getElementById('newsText').value.trim()
-
-//     let messageElement = document.createElement('div');
-//     messageElement.innerHTML = `
-//     <div class="card">
-//     <img src="${photoInput}" alt="News Photo">
-//     <div class="card-container">
-//       <h4>${messageTitle}</h4>
-//       <p>${messageText}</p>
-//     </div>
-//     </div>
-
-//     <hr class="divider">`;
-
-//     alert("Ваше повідомлення опубліковане.");
-//     document.getElementById('photoInput').value = '';
-//     document.getElementById('newsText').value = '';
-//     document.getElementById('titleText').value = '';
-
-//     // document.getElementByClass('news').prepend(messageElement);
-//     newsContainer.insertAdjacentHTML('beforebegin', messageElement );
-//   });
-
-//   // Очищення локального сховища після виведення повідомлень
-//   localStorage.removeItem("messages");
-// });

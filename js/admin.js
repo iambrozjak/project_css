f3ec827e9b2dcb8b26c7efe9cf12bbeb6df9bbc7
iambrozjak <ivanochkavasyljuk@gmail.com>
2024-01-01 23:41:27 +0200

    Add image to new

--- js/admin.js
+++ js/admin.js
@@ -1,34 +1,46 @@
-document.getElementById('adminForm').addEventListener('submit', function(event) {
+document.getElementById('adminForm').addEventListener('submit', function (event) {
   event.preventDefault();
 
   const photoInput = document.getElementById('photoInput');
   const messageTitle = document.getElementById('titleText').value.trim();
-  const messageText = document.getElementById('newsText').value.trim()
+  const messageText = document.getElementById('newsText').value.trim();
 
-  if (!messageTitle || !messageText){
-    alert("Будь-ласка, заповніть поля.");
+  if (!messageTitle || !messageText) {
+    alert("Будь ласка, заповніть поля.");
     return;
   }
-  if (!photoInput.files || !photoInput.files[0]){
-    alert("Будь-ласка, оберіть фото.");
+  if (!photoInput.files || !photoInput.files[0]) {
+    alert("Будь ласка, оберіть фото.");
     return;
   }
 
-  const message = {
-    img: photoInput,
-    title: messageTitle,
-    text: messageText
+  const reader = new FileReader();
+  reader.onloadend = function () {
+    const previewImage = document.createElement('img');
+    previewImage.src = reader.result;
+
+    const message = {
+      img: previewImage.outerHTML,
+      title: messageTitle,
+      text: messageText
+    };
+
+    let messages = JSON.parse(localStorage.getItem('messages')) || [];
+    messages.push(message);
+    localStorage.setItem('messages', JSON.stringify(messages));
+
+    alert("Ваша новина збережена в локальному сховищі.");
+
+    document.getElementById('photoInput').value = '';
+    document.getElementById('newsText').value = '';
+    document.getElementById('titleText').value = '';
+
+    //  додаємо елемент зображення до контейнера
+    const previewContainer = document.getElementById('previewContainer');
+    if (previewContainer) {
+      previewContainer.appendChild(previewImage);
+    }
   };
 
-  let messages = JSON.parse(localStorage.getItem('messages')) || [];
-  messages.push(message);
-  localStorage.setItem('messages', JSON.stringify(messages));
-
-  alert("Ваша новина  збережина в локальному сховищі.");
-
-
-  document.getElementById('photoInput').value = '';
-  document.getElementById('newsText').value = '';
-  document.getElementById('titleText').value = '';
-
-})
+  reader.readAsDataURL(photoInput.files[0]);
+});
--- js/news.js
+++ js/news.js
@@ -1,6 +1,45 @@
+// function isOnline() {
+//   return window.navigator.onLine;
+// }
+// document.addEventListener('DOMContentLoaded', function () {
+//   if (isOnline()) {
+//     displayNewsFromLocalStorage();
+//   }
+
+//   // event listener for online event
+//   window.addEventListener('online', displayNewsFromLocalStorage);
+// });
+// function displayNewsFromLocalStorage(){
+
+//     let messages = JSON.parse(localStorage.getItem("messages")) || [];
+//     messages.forEach((message) => {
+//       const photoInput = message.img;
+//       const messageTitle = message.title;
+//       const messageText = message.text;
+
+//       let messageElement = document.createElement('div');
+//       messageElement.innerHTML = `
+//         <div class="card">
+//           <img src="${photoInput}" alt="News Photo">
+//           <div class="card-container">
+//             <h4>${messageTitle}</h4>
+//             <p>${messageText}</p>
+//           </div>
+//         </div>
+//         <hr class="divider">
+//       `;
+
+//       document.getElementById('newsNew').prepend(messageElement);
+//     });
+//   // Очищення локального сховища після виведення повідомлень
+//     localStorage.removeItem("messages");
+//   };
+
+
 function isOnline() {
   return window.navigator.onLine;
 }
+
 document.addEventListener('DOMContentLoaded', function () {
   if (isOnline()) {
     displayNewsFromLocalStorage();
@@ -9,28 +48,29 @@ document.addEventListener('DOMContentLoaded', function () {
   // event listener for online event
   window.addEventListener('online', displayNewsFromLocalStorage);
 });
-function displayNewsFromLocalStorage(){
 
-    let messages = JSON.parse(localStorage.getItem("messages")) || [];
-    messages.forEach((message) => {
-      const photoInput = message.img;
-      const messageTitle = message.title;
-      const messageText = message.text;
+function displayNewsFromLocalStorage() {
+  let messages = JSON.parse(localStorage.getItem('messages')) || [];
+  messages.forEach((message) => {
+    const photoInputHTML = message.img;
+    const messageTitle = message.title;
+    const messageText = message.text;
 
-      let messageElement = document.createElement('div');
-      messageElement.innerHTML = `
-        <div class="card">
-          <img src="${photoInput}" alt="News Photo">
-          <div class="card-container">
-            <h4>${messageTitle}</h4>
-            <p>${messageText}</p>
-          </div>
+    let messageElement = document.createElement('div');
+    messageElement.innerHTML = `
+      <div class="card">
+        ${photoInputHTML}
+        <div class="card-container">
+          <h4>${messageTitle}</h4>
+          <p>${messageText}</p>
         </div>
-        <hr class="divider">
-      `;
+      </div>
+      <hr class="divider">
+    `;
 
-      document.getElementById('newsNew').prepend(messageElement);
-    });
+    document.getElementById('newsNew').prepend(messageElement);
+  });
+
   // Очищення локального сховища після виведення повідомлень
-    localStorage.removeItem("messages");
-  };
+  localStorage.removeItem('messages');
+}
